bb38fdd2d3eff8840f0bf775de3cf07719501ff7
swyftos <98902001+swyftos@users.noreply.github.com>
2025-05-25 23:31:28 +0200

    Update mini_maths.c

--- testn1/src/mini_maths.c
+++ testn1/src/mini_maths.c
@@ -1,22 +1,32 @@
 #ifndef DEVICE
 // Sur simulateur Linux, on stubbe l’API des ext-apps  
+#include <stdint.h>               // pour uint32_t
 void extapp_clear(void) { }
 void extapp_drawPixel(int x, int y, int on) { }
-// Ajustement : uint32_t pour coller au prototype officiel
-#include <stdint.h>
+// Prototype officiel : void extapp_msleep(uint32_t ms);
 void extapp_msleep(uint32_t ms) { }
 #endif
 
 #include <extapp_api.h>
 #include <math.h>
 
-// --- tes fonctions mathématiques ici ---
+// --- Début fusion maths_lib.c ---
+float sqrt_complex(float a, float b) {
+  return sqrtf(a*a + b*b);
+}
+float derivative_of_fx(float x) {
+  float h = 0.001f;
+  float f1 = x*x + 3*x + 1;
+  float f2 = (x+h)*(x+h) + 3*(x+h) + 1;
+  return (f2 - f1) / h;
+}
+// --- Fin fusion ---
 
 void extapp_main(void) {
   float module = sqrt_complex(3.0f, -2.0f);
   float derivee = derivative_of_fx(2.0f);
-  (void)module;
-  (void)derivee;
+  (void)module;   // évite le warning « unused-variable »
+  (void)derivee;  // idem
 
   extapp_clear();
   extapp_drawPixel(10, 10, 1);
