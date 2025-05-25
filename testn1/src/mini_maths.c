f71f4462a6d204e205fa852da1f6eb39f40ba37e
swyftos <98902001+swyftos@users.noreply.github.com>
2025-05-25 23:30:21 +0200

    Update mini_maths.c

--- testn1/src/mini_maths.c
+++ testn1/src/mini_maths.c
@@ -2,29 +2,21 @@
 // Sur simulateur Linux, on stubbe l’API des ext-apps  
 void extapp_clear(void) { }
 void extapp_drawPixel(int x, int y, int on) { }
-void extapp_msleep(int ms) { }
+// Ajustement : uint32_t pour coller au prototype officiel
+#include <stdint.h>
+void extapp_msleep(uint32_t ms) { }
 #endif
 
 #include <extapp_api.h>
 #include <math.h>
 
-// --- Début fusion maths_lib.c ---
-float sqrt_complex(float a, float b) {
-  return sqrtf(a*a + b*b);
-}
-float derivative_of_fx(float x) {
-  float h = 0.001f;
-  float f1 = x*x + 3*x + 1;
-  float f2 = (x+h)*(x+h) + 3*(x+h) + 1;
-  return (f2 - f1) / h;
-}
-// --- Fin fusion ---
+// --- tes fonctions mathématiques ici ---
 
 void extapp_main(void) {
   float module = sqrt_complex(3.0f, -2.0f);
   float derivee = derivative_of_fx(2.0f);
-  (void)module;   // évite le warning « unused-variable »
-  (void)derivee;  // idem
+  (void)module;
+  (void)derivee;
 
   extapp_clear();
   extapp_drawPixel(10, 10, 1);
